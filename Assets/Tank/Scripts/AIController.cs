bd28b9fbc46a32beb0b3a38349f7787735acff73
Quixatocs <quixatocs@gmail.com>
2020-09-30 14:35:20 +0100

    implementing singleton pattern for the computer and the player controller

--- Assets/Tank/Scripts/AIController.cs
+++ Assets/Tank/Scripts/AIController.cs
@@ -1,4 +1,29 @@
-﻿public class AIController : StateMachine
+﻿using System;
+
+public class AIController : StateMachine
 {
     
+    #region Singleton Instance
+
+    private static AIController instance;
+
+    #endregion
+    
+    #region Monobehaviour Methods
+
+    private void Awake() {
+        // Singleton implementation
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+    }
+
+    private void Start() {
+        //TODO transition to the opening state
+    }
+    
+    #endregion
 }
--- Assets/Tank/Scripts/PlayerController.cs
+++ Assets/Tank/Scripts/PlayerController.cs
@@ -1,4 +1,27 @@
 ﻿public class PlayerController : StateMachine
 {
     
+    #region Singleton Instance
+
+    private static PlayerController instance;
+
+    #endregion
+    
+    #region Monobehaviour Methods
+
+    private void Awake() {
+        // Singleton implementation
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+    }
+
+    private void Start() {
+        //TODO transition to the opening state
+    }
+    
+    #endregion
 }
