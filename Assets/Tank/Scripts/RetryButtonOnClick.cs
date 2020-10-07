cd09f88c6f640545c545d51e008b619894360383
Quixatocs <quixatocs@gmail.com>
2020-10-07 15:06:26 +0100

    adding a retry option

---
+++ Assets/Tank/Scripts/RetryButtonOnClick.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+/// <summary>
+/// Monobehaviour to reload the scene on click
+/// </summary>
+[RequireComponent(typeof(Button))]
+public class RetryButtonOnClick : MonoBehaviour
+{
+
+    #region Monobehaviour Methods
+
+    private void OnEnable() {
+        GetComponent<Button>().onClick.AddListener(ReloadScene);
+    }
+
+    private void OnDisable() {
+        GetComponent<Button>().onClick.RemoveAllListeners();
+    }
+
+
+    #endregion
+
+    #region Private Methods
+
+    private void ReloadScene() {
+        SceneManager.LoadScene(0);
+    }
+
+    #endregion
+    
+}
